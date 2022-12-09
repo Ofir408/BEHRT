25e92f659354b58b1ca792aface08a7793519ec8
Ofir <ofir408@gmail.com>
2022-12-09 20:57:14 +0200

    removed sex embeddings

--- model/MLM.py
+++ model/MLM.py
@@ -14,33 +14,29 @@ class BertEmbeddings(nn.Module):
         self.age_embeddings = nn.Embedding(config.age_vocab_size, config.hidden_size)
         self.posi_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size). \
             from_pretrained(embeddings=self._init_posi_embedding(config.max_position_embeddings, config.hidden_size))
-        self.sex_embeddings = nn.Embedding(config.sex_vocab_size, config.hidden_size) #local
-
         self.LayerNorm = Bert.modeling.BertLayerNorm(config.hidden_size, eps=1e-12)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, word_ids, age_ids=None, seg_ids=None, posi_ids=None, age=True, sex_ids=None):
+    def forward(self, word_ids, age_ids=None, seg_ids=None, posi_ids=None, age=True):
         if seg_ids is None:
             seg_ids = torch.zeros_like(word_ids)
         if age_ids is None:
             age_ids = torch.zeros_like(word_ids)
         if posi_ids is None:
             posi_ids = torch.zeros_like(word_ids)
-        if sex_ids is None:
-            sex_ids = torch.zeros_like(word_ids)
 
-
+        #print(f'age_ids={age_ids}')
         word_embed = self.word_embeddings(word_ids)
         segment_embed = self.segment_embeddings(seg_ids)
         age_embed = self.age_embeddings(age_ids)
         posi_embeddings = self.posi_embeddings(posi_ids)
-        sex_embed = self.sex_embeddings(sex_ids)
 
         if age:
-            embeddings = word_embed + segment_embed + age_embed + sex_embed + posi_embeddings
+            embeddings = word_embed + segment_embed + age_embed  + posi_embeddings
         else:
-            embeddings = word_embed + segment_embed + sex_embed + posi_embeddings
+            embeddings = word_embed + segment_embed + posi_embeddings
         embeddings = self.LayerNorm(embeddings)
+        #print(embeddings)
         embeddings = self.dropout(embeddings)
         return embeddings
 
@@ -75,7 +71,7 @@ class BertModel(Bert.modeling.BertPreTrainedModel):
         self.pooler = Bert.modeling.BertPooler(config)
         self.apply(self.init_bert_weights)
 
-    def forward(self, input_ids, age_ids=None, seg_ids=None, posi_ids=None, sex_ids=None, attention_mask=None,
+    def forward(self, input_ids, age_ids=None, seg_ids=None, posi_ids=None, attention_mask=None,
                 output_all_encoded_layers=True):
         if attention_mask is None:
             attention_mask = torch.ones_like(input_ids)
@@ -85,8 +81,6 @@ class BertModel(Bert.modeling.BertPreTrainedModel):
             seg_ids = torch.zeros_like(input_ids)
         if posi_ids is None:
             posi_ids = torch.zeros_like(input_ids)
-        if sex_ids is None:
-            sex_ids = torch.zeros_like(input_ids)
 
         # We create a 3D attention mask from a 2D tensor mask.
         # Sizes are [batch_size, 1, 1, to_seq_length]
@@ -103,7 +97,7 @@ class BertModel(Bert.modeling.BertPreTrainedModel):
         extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
 
-        embedding_output = self.embeddings(input_ids, age_ids, seg_ids, posi_ids, sex_ids)
+        embedding_output = self.embeddings(word_ids=input_ids, age_ids=age_ids, seg_ids=seg_ids, posi_ids=posi_ids, age=True)
         encoded_layers = self.encoder(embedding_output,
                                       extended_attention_mask,
                                       output_all_encoded_layers=output_all_encoded_layers)
@@ -121,8 +115,8 @@ class BertForMaskedLM(Bert.modeling.BertPreTrainedModel):
         self.cls = Bert.modeling.BertOnlyMLMHead(config, self.bert.embeddings.word_embeddings.weight)
         self.apply(self.init_bert_weights)
 
-    def forward(self, input_ids, age_ids=None, seg_ids=None, posi_ids=None, sex_ids=None, attention_mask=None, masked_lm_labels=None):
-        sequence_output, _ = self.bert(input_ids, age_ids, seg_ids, posi_ids, sex_ids, attention_mask,
+    def forward(self, input_ids, age_ids=None, seg_ids=None, posi_ids=None, attention_mask=None, masked_lm_labels=None):
+        sequence_output, _ = self.bert(input_ids=input_ids, age_ids=age_ids, seg_ids=seg_ids, posi_ids=posi_ids, attention_mask=attention_mask,
                                        output_all_encoded_layers=False)
         prediction_scores = self.cls(sequence_output)
 
